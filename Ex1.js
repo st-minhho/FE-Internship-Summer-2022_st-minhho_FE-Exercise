6f1268e0d100d100319669126aec478020577874
st-minhho <minh.ho@supremetech.vn>
2022-06-17 16:14:36 +0700

    use single quote

--- Ex1.js
+++ Ex1.js
@@ -1,21 +1,21 @@
-const tabLink = document.querySelectorAll(".tab-link");
-const tabContent = document.querySelectorAll(".tab-content");
+const tabLink = document.querySelectorAll('.tab-link');
+const tabContent = document.querySelectorAll('.tab-content');
 function openTab() {
   for (let i = 0; i < tabLink.length; i++) {
     tabLink[i].onclick = function (e) {
       for (let i = 0; i < tabLink.length; i++) {
-        if (tabLink[i].classList.contains("current")) {
-          tabLink[i].classList.remove("current");
+        if (tabLink[i].classList.contains('current')) {
+          tabLink[i].classList.remove('current');
         }
       }
-      tabLink[i].classList.add("current");
+      tabLink[i].classList.add('current');
       let tabID = document.getElementById(e.target.dataset.tab);
       for (let j = 0; j < tabContent.length; j++) {
-        if (tabContent[j].classList.contains("current")) {
-          tabContent[j].classList.remove("current");
+        if (tabContent[j].classList.contains('current')) {
+          tabContent[j].classList.remove('current');
         }
       }
-      tabID.classList.add("current");
+      tabID.classList.add('current');
     };
   }
 }
