a0c620b37b4cc8adc04b43cb314afedf923649e2
st-minhho <minh.ho@supremetech.vn>
2022-06-20 09:56:20 +0700

    edit to single quote

--- addToCart.js
+++ addToCart.js
@@ -1,80 +1,51 @@
-const product = document.querySelector(".product-list");
-const totalCount = document.querySelector(".total-cart");
+const product = document.querySelector('.product-list');
+const totalCount = document.querySelector('.total-cart');
 var productList = {};
 var productItem = {};
 var cartQty = 0;
 const productData = [
   {
-    id: "pd1",
-    name: "T-Shirt Summer Vibes",
-    price: "89.99",
-    imgSrc: "./images/image-product1.png",
+    id: 'pd1',
+    name: 'T-Shirt Summer Vibes',
+    price: '89.99',
+    imgSrc: './images/image-product1.png',
   },
   {
-    id: "pd2",
-    name: "Loose Knit 3/4 Sleeve",
-    price: "119.99",
-    imgSrc: "./images/image-product2.png",
+    id: 'pd2',
+    name: 'Loose Knit 3/4 Sleeve',
+    price: '119.99',
+    imgSrc: './images/image-product2.png',
   },
   {
-    id: "pd3",
-    name: "Basic Slim Fit T-Shirt",
-    price: "119.99",
-    imgSrc: "./images/image-product3.png",
+    id: 'pd3',
+    name: 'Basic Slim Fit T-Shirt',
+    price: '119.99',
+    imgSrc: './images/image-product3.png',
   },
   {
-    id: "pd4",
-    name: "Loose Textured T-Shirt",
-    price: "119.99",
-    imgSrc: "./images/image-product4.png",
+    id: 'pd4',
+    name: 'Loose Textured T-Shirt',
+    price: '119.99',
+    imgSrc: './images/image-product4.png',
   },
 ];
-localStorage.setItem("product", JSON.stringify(productData));
+localStorage.setItem('product', JSON.stringify(productData));
 
 eventListeners();
 function eventListeners() {
-  window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener('DOMContentLoaded', () => {
     renderData();
     renderTotalCart();
   });
 
-  product.addEventListener("click", addToLocal);
+  product.addEventListener('click', addToLocal);
 }
 
-//reder prodct item
-// function renderData() {
-//   fetch("products.json")
-//     .then((respone) => respone.json())
-//     .then((data) => {
-//       var html = "";
-//       data.forEach((product) => {
-//         html += `
-//         <li class="col-3 product-item">
-//           <div class="product-img">
-//             <img id=${product.id} src="${product.imgSrc}" alt="Image product" />
-//             <div class="product-overlay">
-//                 <a href="#" class="btn btn-primary add-to-cart" >Add to cart</a>
-//               </div>
-//           </div>
-//           <div class="product-content">
-//             <h4 class="product-name">${product.name}</h4>
-//             <div class="product-price">
-//               <span class="price-sell">$${product.price}</span>
-//             </div>
-//           </div>
-//         </li>
-//         `;
-//       });
-//       product.innerHTML = html;
-//     });
-
-// }
-
 function renderData() {
-  var data = localStorage.getItem("product");
+  var data = localStorage.getItem('product');
   if (data) {
     var productData = JSON.parse(data);
-    var html = "";
+    var html = '';
     Object.keys(productData).map((key, value) => {
       html += `
           <li class="col-3 product-item">
@@ -97,30 +68,28 @@ function renderData() {
   }
 }
 function renderTotalCart() {
-  var cartTotal = JSON.parse(localStorage.getItem("cartTotal"));
+  var cartTotal = JSON.parse(localStorage.getItem('cartTotal'));
   totalCount.innerHTML = cartTotal;
 }
 
 function addToLocal(e) {
   //get product information
-  if (e.target.classList.contains("add-to-cart")) {
+  if (e.target.classList.contains('add-to-cart')) {
     var product = e.target.parentElement.parentElement.parentElement;
-    var getID = product.querySelector(".product-img img").getAttribute("id");
-    var getName = product.querySelector(".product-name").textContent;
-    var getImgSrc = product
-      .querySelector(".product-img img")
-      .getAttribute("src");
-    var getPrice = product.querySelector(".price-sell").textContent;
+    var getID = product.querySelector('.product-img img').getAttribute('id');
+    var getName = product.querySelector('.product-name').textContent;
+    var getImgSrc = product.querySelector('.product-img img').getAttribute('src');
+    var getPrice = product.querySelector('.price-sell').textContent;
     var getQty = 1;
   }
 
-  productList = localStorage.getItem("cart");
+  productList = localStorage.getItem('cart');
   if (productList) {
     productItem = JSON.parse(productList);
 
     Object.keys(productItem).map((key, value) => {
       if (key == getID) {
-        getQty = productItem[key]["qty"] + 1;
+        getQty = productItem[key]['qty'] + 1;
       }
     });
   }
@@ -132,19 +101,19 @@ function addToLocal(e) {
     qty: getQty,
   };
 
-  localStorage.setItem("cart", JSON.stringify(productItem));
+  localStorage.setItem('cart', JSON.stringify(productItem));
   totalCart();
 }
 
 function totalCart() {
   var cartTotal = 0;
-  productList = localStorage.getItem("cart");
+  productList = localStorage.getItem('cart');
   if (productList) {
     productItem = JSON.parse(productList);
     Object.keys(productItem).map((key, value) => {
-      cartTotal += productItem[key]["qty"];
+      cartTotal += productItem[key]['qty'];
     });
   }
-  localStorage.setItem("cartTotal", JSON.stringify(cartTotal));
+  localStorage.setItem('cartTotal', JSON.stringify(cartTotal));
   totalCount.innerHTML = cartTotal;
 }
--- cart.js
+++ cart.js
@@ -1,28 +1,28 @@
-const productTableList = document.querySelector(".product-cart-list");
-const totalCount = document.querySelector(".total-cart");
+const productTableList = document.querySelector('.product-cart-list');
+const totalCount = document.querySelector('.total-cart');
 var total = 0;
 var subTotal = 0;
 var sum = 0;
 eventListeners();
 function eventListeners() {
-  window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener('DOMContentLoaded', () => {
     renderData();
     renderTotalCart();
   });
-  productTableList.addEventListener("click", increaseQuantity);
-  productTableList.addEventListener("click", reduceQuantity);
-  productTableList.addEventListener("click", delProduct);
+  productTableList.addEventListener('click', increaseQuantity);
+  productTableList.addEventListener('click', reduceQuantity);
+  productTableList.addEventListener('click', delProduct);
 }
 
 function renderData() {
-  var productList = localStorage.getItem("cart");
+  var productList = localStorage.getItem('cart');
   if (productList) {
     productInCart = JSON.parse(productList);
     
     Object.keys(productInCart).map((key, value) => {
-      var html = "";
-      var price = productInCart[key]["price"].slice(1);
-      var qty = productInCart[key]["qty"];
+      var html = '';
+      var price = productInCart[key]['price'].slice(1);
+      var qty = productInCart[key]['qty'];
       sum = price * qty;
       subTotal = sum.toFixed(2);
       total += parseFloat(subTotal);
@@ -36,18 +36,18 @@ function renderData() {
           </td>
           <td class="image-prod">
             <div class="product-img">
-              <img src="${productInCart[key]["imgSrc"]}" alt="">
+              <img src="${productInCart[key]['imgSrc']}" alt="">
             </div>
           </td>
           <td class="product-name">
-            <h4>${productInCart[key]["name"]}</h4>
+            <h4>${productInCart[key]['name']}</h4>
             <span class="product-id">${key}<span>
           </td>
-          <td class="price">${productInCart[key]["price"]}</td>
+          <td class="price">${productInCart[key]['price']}</td>
           <td class="quantity">
             <div class="cart_quantity_button">
               <a class="cart-quantity-up" href="#"> + </a>
-              <input class="cart_quantity_input" type="text" name="quantity" value="${productInCart[key]["qty"]}">
+              <input class="cart_quantity_input" type="text" name="quantity" value="${productInCart[key]['qty']}">
               <a class="cart-quantity-down" href="#"> - </a>
             </div>
           </td>
@@ -56,93 +56,89 @@ function renderData() {
       `;
       productTableList.innerHTML += html;
     });
-    document.getElementById("price-total").innerHTML = "$" + total;
+    document.getElementById('price-total').innerHTML = '$' + total;
   }
 }
 
 function renderTotalCart() {
-  var cartTotal = JSON.parse(localStorage.getItem("cartTotal"));
+  var cartTotal = JSON.parse(localStorage.getItem('cartTotal'));
   totalCount.innerHTML = cartTotal;
 }
 function totalCart() {
   var cartTotal = 0;
-  var productList = localStorage.getItem("cart");
+  var productList = localStorage.getItem('cart');
   if (productList) {
     productItem = JSON.parse(productList);
     Object.keys(productItem).map((key, value) => {
-      cartTotal += productItem[key]["qty"];
+      cartTotal += productItem[key]['qty'];
     });
   }
-  localStorage.setItem("cartTotal", JSON.stringify(cartTotal));
+  localStorage.setItem('cartTotal', JSON.stringify(cartTotal));
   totalCount.innerHTML = cartTotal;
 }
 
 function increaseQuantity(e) {
-  if (e.target.classList.contains("cart-quantity-up")) {
+  if (e.target.classList.contains('cart-quantity-up')) {
     var product = e.target.parentElement.parentElement.parentElement;
-    var getID = product.querySelector(".product-id").innerText;
-    var getQty = product
-      .querySelector(".cart_quantity_input")
-      .getAttribute("value");
-    var getPrice = product.querySelector(".price").textContent.slice(1);
+    var getID = product.querySelector('.product-id').innerText;
+    var getQty = product.querySelector('.cart_quantity_input').getAttribute('value');
+    var getPrice = product.querySelector('.price').textContent.slice(1);
   }
 
   getQty = parseInt(getQty) + 1;
   sum = getQty * parseFloat(getPrice);
   subTotal = sum.toFixed(2);
 
-  total = document.getElementById("price-total").textContent.slice(1);
+  total = document.getElementById('price-total').textContent.slice(1);
   total = parseFloat(total) + parseFloat(getPrice);
-  document.getElementById("price-total").innerHTML = "$" + total;
-  var productList = localStorage.getItem("cart");
+  console.log(total)
+  document.getElementById('price-total').innerHTML = '$' + total;
+  console.log(document.getElementById('price-total').innerHTML)
+  var productList = localStorage.getItem('cart');
   if (productList) {
     var productItem = JSON.parse(productList);
     Object.keys(productItem).map((key, value) => {
       if (key === getID) {
-        productItem[key]["qty"]++;
-        product
-          .querySelector(".cart_quantity_input")
-          .setAttribute("value", productItem[key]["qty"]);
-        product.querySelector(".total").innerHTML = "$" + subTotal;
+        productItem[key]['qty']++;
+        product.querySelector('.cart_quantity_input').setAttribute('value', productItem[key]['qty']);
+        product.querySelector('.total').innerHTML = '$' + subTotal;
       }
     });
   }
-  localStorage.setItem("cart", JSON.stringify(productItem));
+  localStorage.setItem('cart', JSON.stringify(productItem));
   totalCart();
 }
 
 function reduceQuantity(e) {
-  if (e.target.classList.contains("cart-quantity-down")) {
+  if (e.target.classList.contains('cart-quantity-down')) {
     var product = e.target.parentElement.parentElement.parentElement;
-    var getID = product.querySelector(".product-id").innerText;
+    var getID = product.querySelector('.product-id').innerText;
     var getQty = product
-      .querySelector(".cart_quantity_input")
-      .getAttribute("value");
-    var getPrice = product.querySelector(".price").textContent.slice(1);
+      .querySelector('.cart_quantity_input')
+      .getAttribute('value');
+    var getPrice = product.querySelector('.price').textContent.slice(1);
   }
 
   if (getQty > 1) {
     getQty = parseInt(getQty) - 1;
     sum = getQty * parseFloat(getPrice);
     subTotal = sum.toFixed(2);
-    product.querySelector(".total").innerHTML = "$" + subTotal;
+    product.querySelector('.total').innerHTML = '$' + subTotal;
   }
 
-  total = document.getElementById("price-total").textContent.slice(1);
+  total = document.getElementById('price-total').textContent.slice(1);
   console.log(total)
   total = parseFloat(total) - parseFloat(getPrice);
-  document.getElementById("price-total").innerHTML = "$" + total;
+  document.getElementById('price-total').innerHTML = '$' + total;
 
-  var productList = localStorage.getItem("cart");
+  var productList = localStorage.getItem('cart');
   if (productList) {
     var productItem = JSON.parse(productList);
     Object.keys(productItem).map((key, value) => {
       if (key === getID) {
-        if (productItem[key]["qty"] > 1) {
-          productItem[key]["qty"]--;
-          product
-            .querySelector(".cart_quantity_input")
-            .setAttribute("value", productItem[key]["qty"]);
+        if (productItem[key]['qty'] > 1) {
+          productItem[key]['qty']--;
+          product.querySelector('.cart_quantity_input').setAttribute('value', productItem[key]['qty']);
         } else {
           delete productItem[key];
           product.parentNode.removeChild(product);
@@ -150,16 +146,16 @@ function reduceQuantity(e) {
       }
     });
   }
-  localStorage.setItem("cart", JSON.stringify(productItem));
+  localStorage.setItem('cart', JSON.stringify(productItem));
   totalCart();
 }
 
 function delProduct(e) {
-  if (e.target.classList.contains("remove-link")) {
+  if (e.target.classList.contains('remove-link')) {
     var product = e.target.parentElement.parentElement;
-    var getID = product.querySelector(".product-id").innerText;
+    var getID = product.querySelector('.product-id').innerText;
   }
-  var productList = localStorage.getItem("cart");
+  var productList = localStorage.getItem('cart');
   if (productList) {
     var productItem = JSON.parse(productList);
     Object.keys(productItem).map((key, value) => {
@@ -169,7 +165,7 @@ function delProduct(e) {
       }
     });
   }
-  localStorage.setItem("cart", JSON.stringify(productItem));
+  localStorage.setItem('cart', JSON.stringify(productItem));
   totalCart();
 }
 
